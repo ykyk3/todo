2b95f6c9b1b742948804e9b01d27b955ddeb564c
NEC 甲元 <bs-komoto.yoshiro@kcc.co.jp>
2019-07-10 00:14:35 +0900

    A complete set of messages API

--- functions/src/routes/message.ts
+++ functions/src/routes/message.ts
@@ -5,15 +5,35 @@ const app = express();
 admin.initializeApp();
 
 app.get('/', async (req, res) => {
-  // grab the text parameter.
-  // push the new message into the realtime database using the firebase admin sdk.
-  admin.database().ref('/messages').on('value', (snapshot: any) => {
-    // res.send({test:'test'})
-    res.send(JSON.stringify(snapshot.val()));
-  });
+  admin.database().ref('/messages').on('value', (data: any) => {
+    res.status(200).send(data);
+  })
+});
+
+app.get('/:id', async (req, res) => {
+  const id: string = req.params.id;
+  admin.database().ref(`/messages/${id}`).on('value', (data: any) => {
+    res.status(200).send(data);
+  })
 });
 
 app.post('/', async (req, res) => {
+  const original = req.body.message;
+  // push the new message into the realtime database using the firebase admin sdk.
+  const snapshot = await admin.database().ref('/messages').push({ original: original });
+  // redirect with 303 see other to the url of the pushed object in the firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
+app.put('/', async (req, res) => {
+  const original = req.body.message;
+  // push the new message into the realtime database using the firebase admin sdk.
+  const snapshot = await admin.database().ref('/messages').push({ original: original });
+  // redirect with 303 see other to the url of the pushed object in the firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
+app.delete('/', async (req, res) => {
   const original = req.body.message;
   // push the new message into the realtime database using the firebase admin sdk.
   const snapshot = await admin.database().ref('/messages').push({ original: original });
